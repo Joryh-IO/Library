7f1ffe80dcf152ecf8305d54e0f5754b05dd2261
George <sujeethvizag@gmail.com>
2024-12-10 10:36:22 +0530

    Update StudentList.java

--- StudentList.java
+++ StudentList.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class StudentList {
+//    Map<Integer, Student> m = new HashMap<Integer,Student>();
     ArrayList<Student> s = new ArrayList<Student>();
     StudentList(){
         Student a = new Student("Cassy", 101);
@@ -9,6 +13,9 @@ public class StudentList {
         s.add(a);
         s.add(b);
         s.add(c);
+//        m.put(a.id,a);
+//        m.put(b.id,b);
+//        m.put(c.id,c);
     }
 
     public void register(Student a){
@@ -20,5 +27,26 @@ public class StudentList {
         }
     }
 
-    public void search()
+    public boolean search(String a, int b){
+        int c=0;
+        for(Student x: s){
+            if(b == x.id && a.equals(x.name))return true;
+            else c++;
+        }
+        if(c!=0)return false;
+        else return true;
+    }
+
+    public void removeStudent(String name, int id){
+        int c=0;
+        for(Student x: s){
+            if(id == x.id && name.equals(x.name)) {
+                s.remove(x);
+                return;
+            }
+            else c++;
+        }
+        if(c!=0)System.out.println("Student not found");
+
+    }
 }
